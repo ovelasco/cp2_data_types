75ad01f042f42f348bce15638016769fe7691e62
Omar Velasco <ovelasco@chicagotechacademy.org>
2014-10-03 12:49:11 -0500

    Update data_types.rb

--- data_types.rb
+++ data_types.rb
@@ -1,11 +1,11 @@
-kaykay = { :age => 21, :height => "5'6\"" }
+omar = { :age => 16, :height => "6'0\"" }
 
-def say_kaykay
-  "KayKay"
+def say_omar
+  "Omar"
 end
 
 space = " "
 
-puts say_kaykay + " is" + space + kaykay[:age].to_s + "."
+puts say_omar + " is" + space + kaykay[:age].to_s + "."
 
-puts say_kaykay + " is" + space + kaykay[:height] + "."
+puts say_omar + " is" + space + kaykay[:height] + "."
